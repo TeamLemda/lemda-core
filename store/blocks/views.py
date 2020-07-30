462309f423b529e0cecbafa86d2a6d2054adbb3b
Ofek Censor <ofek.censor@gmail.com>
2020-07-30 19:12:56 +0300

    Added view block stubs

---
+++ store/blocks/views.py
@@ -0,0 +1,15 @@
+"""
+These are view functions, they live here as stubs so the curation API can tell they exist.
+"""
+
+def lemda_block_function_field(field_name, function, variable, correct, feedback, validation_feedback, grade, show_correct, student_answer, **state):
+	"""
+	"""
+
+def lemda_block_graph_point_field(field_name, correct, feedback, validation_feedback, grade, show_correct, student_answer, **state):
+	"""
+	"""
+
+def lemda_block_html(content, grade, is_submit, **state):
+	"""
+	"""

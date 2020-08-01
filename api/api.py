5d24720323a452d4a386ed0fc030233b0bcd21bd
Ofek Censor <ofek.censor@gmail.com>
2020-08-01 21:25:56 +0300

    Bypassing bug in flask-restx

--- api/api.py
+++ api/api.py
@@ -1,13 +1,25 @@
 import logging
 import traceback
 
+import flask
 from flask_restx import Api
 import settings
 
 log = logging.getLogger(__name__)
 
+@property
+def specs_url(self):
+    """Fixes issue where swagger-ui makes a call to swagger.json over HTTP.
+       This can ONLY be used on servers that actually use HTTPS.  On servers that use HTTP,
+       this code should not be used at all.
+    """
+    return flask.url_for(self.endpoint('specs'), _external=False)
+
+Api.specs_url = specs_url
 api = Api(version="0.1", title="Lemda API",
           description="Lemda Question API")
+
+
 
 def create_message(content):
 	return {
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging.config
 import os
 from flask import Flask, Blueprint
 from flask_cors import CORS
-from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 import settings
 from api.endpoints.curate import ns as curate_namespace
@@ -10,12 +10,11 @@ from api.endpoints.questions import ns as questions_namespace
 from api import api
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_port=1, x_for=1, x_host=1, x_prefix=1)
+
 
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "logging.conf"))
 logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
-
 
 def configure_app(flask_app):
     """
@@ -26,6 +25,7 @@ def configure_app(flask_app):
     flask_app.config["RESTX_VALIDATE"] = settings.RESTX_VALIDATE
     flask_app.config["RESTX_MASK_SWAGGER"] = settings.RESTX_MASK_SWAGGER
     flask_app.config["ERROR_404_HELP"] = settings.RESTX_ERROR_404_HELP
+
 
 def initialize_app(flask_app):
     """
